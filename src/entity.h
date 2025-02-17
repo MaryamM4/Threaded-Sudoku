2c10854cab52b35f7cebf133b4f86625aa5924c2
MaryamM4 <maryamma@outlook.com>
2025-02-17 04:36:02 +0000

    Added sudoku solve

---
+++ src/entity.h
@@ -0,0 +1,283 @@
+
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// MIN credit: https://stackoverflow.com/a/3437433
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+#define MIN3(x, y, z) (MIN(MIN((x), (y)), (z))) // MIN3: ChatGPT
+
+#define NUM_HOUSE_TYPES 3
+typedef enum { box_t_idx = 0, row_t_idx, col_t_idx } HOUSE_T_IDX;
+
+extern int ORDER; // Number of cols/rows in the grid
+
+typedef enum status { INVALID = -1, FULL = 0 } status;
+
+// Idx 0 of a House is reserved for a candidate count
+// Every other idx represents a value, and stores a count
+// of the number of times it appear in the House.
+// E.g., if House[3] = 1, there is one '3' in the House.
+
+// Idx 0: Count of total possible. Idx 1-ORDER, count of value within house.
+typedef int *House;    // Box/col/row. 1-idxed (0 reserved for candidate count)
+typedef House *Houses; // 0-indexed
+
+typedef struct {
+  int order;                      // Number of cols/rows/boxes in grid.
+  Houses houses[NUM_HOUSE_TYPES]; // 0: boxes, 1: rows, 2: col houses.
+  bool valid, complete;
+} Grid;
+
+// Needed for thread arguments.
+typedef struct {
+  Grid *grid;
+  int house_type_idx, house_idx;
+} GridIterator;
+
+// =========
+// "De/Constructors"
+// --------
+
+House initEmptyHouse() {
+  House hs = (House)malloc((ORDER + 1) * sizeof(int));
+  if (hs == NULL) {
+    perror("initEmptyHouse Error: Failed to allocate memory.");
+    exit(EXIT_FAILURE);
+  }
+
+  hs[0] = ORDER;
+  for (int val = 1; val <= ORDER; val++) {
+    hs[val] = 0;
+  }
+
+  return hs;
+}
+
+Houses initEmptyHouses() {
+  Houses hss = (Houses)malloc(ORDER * sizeof(House));
+  if (hss == NULL) {
+    perror("initEmptyHouses Error: Failed to allocate memory.");
+    exit(EXIT_FAILURE);
+  }
+
+  for (int hs_idx = 0; hs_idx < ORDER; hs_idx++) {
+    hss[hs_idx] = initEmptyHouse();
+  }
+
+  return hss;
+}
+
+Grid *newGrid() {
+  Grid *grid = (Grid *)malloc(sizeof(Grid));
+  if (!grid) {
+    printf("newGrid Error: Failed to allocate grid.");
+    exit(EXIT_FAILURE);
+  }
+
+  grid->order = 0;
+  grid->valid = false;
+  grid->complete = false;
+
+  for (int i = 0; i < NUM_HOUSE_TYPES; i++) {
+    grid->houses[i] = NULL;
+  }
+  return grid;
+}
+
+GridIterator *newGridIt(Grid *grid, int hs_t_idx, int hs_idx) {
+  if (hs_t_idx < -1 || hs_t_idx >= NUM_HOUSE_TYPES) {
+    printf("newGridIt Error: house_type_index %d out of bounds. Must be -1, or "
+           "0-%d.\n",
+           hs_t_idx, NUM_HOUSE_TYPES - 1);
+    exit(EXIT_FAILURE);
+  }
+
+  if (hs_idx < -1 || hs_idx >= ORDER) {
+    printf(
+        "newGridIt Error: house_index %d out of bounds. Must be -1, or 0-%d.\n",
+        hs_idx, ORDER - 1);
+    exit(EXIT_FAILURE);
+  }
+
+  GridIterator *grid_it = (GridIterator *)malloc(sizeof(GridIterator));
+  if (!grid_it) {
+    printf("newGridIt Error: Failed to allocate memory for GridIterator.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  grid_it->grid = grid;
+  grid_it->house_type_idx = hs_t_idx;
+  grid_it->house_idx = hs_idx;
+
+  return grid_it;
+}
+
+// Through iterating through house types
+GridIterator *newGridTypeIt(Grid *grid, int hs_t_idx) {
+  return newGridIt(grid, hs_t_idx, -1);
+}
+
+// For iterating through indexes in houses
+GridIterator *newGridHouseIt(Grid *grid, int hs_idx) {
+  return newGridIt(grid, -1, hs_idx);
+}
+
+void del(Houses *hss) {
+  for (int i = 0; i < ORDER; i++) {
+    free((*hss)[i]);
+    (*hss)[i] = NULL;
+  }
+  free(*hss);
+  *hss = NULL;
+}
+
+void delGrid(Grid *grid) {
+  for (int i = 0; i < NUM_HOUSE_TYPES; i++) {
+    del(&(grid->houses[i]));
+  }
+  free(grid);
+}
+
+// =========
+// Manipulators
+// --------
+
+void addValue(House hs, int val) {
+  if (!hs) {
+    printf("addValue error: NULL House.\n");
+    return;
+  }
+
+  if (val < 0 || val > ORDER) {
+    printf("addValue warning: %d out of bounds.\n", val);
+    hs[0] = INVALID;
+
+  } else if (val > 0) {
+    hs[val] += 1; // If not duplicate, House has one less candidate.
+    hs[0] = (hs[0] == INVALID || hs[val] > 1) ? INVALID : hs[0] - 1;
+  }
+}
+
+void removeValue(House hs, int val) {
+  if (!(hs) || val > ORDER) {
+    printf("addValue Error: NULL House.");
+    return;
+  }
+
+  if (val < 0 || val > ORDER) {
+    printf("removeValue warning:  %d out of bounds.\n", val);
+    hs[0] = INVALID;
+
+  } else if (val > 0) {
+    hs[val] -= 1; // If valid, House has one more candidate.
+    hs[0] = (hs[0] == INVALID || hs[val] > 1) ? INVALID : hs[0] + 1;
+  }
+}
+
+// When a candidate is determined as an answer
+// Add value to corresponding houses.
+void setValue(int val, Grid grid, int row, int col) {
+  int box = getBoxIdx(row, col, grid.order);
+
+  addValue(grid.houses[box_t_idx][box], val);
+  addValue(grid.houses[row_t_idx][row], val);
+  addValue(grid.houses[col_t_idx][col], val);
+}
+
+// =========
+// Set Operations
+// --------
+
+// In sudoku, a locked candidate is when a canditade is restricted to a specific
+// row/col in a box, and it can be eliminated from other rows/cols out that box.
+int *locked_candidate_pairs(House hsA, House hsB) {
+  int max_locked = MIN(hsA[0], hsB[0]);
+
+  if (max_locked < 0) {
+    perror("locked_candidates Error: Invalid House(s).");
+    return NULL;
+  }
+
+  int *locked = (int *)malloc((max_locked + 1) * sizeof(int));
+  if (!locked) {
+    printf("locked_candidate_pairs Error: Failed to allocate memory.");
+    exit(EXIT_FAILURE);
+  }
+
+  locked[0] = 0; // First index reserved for candidate count.
+
+  for (int val = 1; val <= ORDER; val++) {
+    if (hsA[val] == 0 && hsB[val] == 0) {
+      locked[locked[0] + 1] = val;
+      locked[0]++;
+    }
+  }
+  return locked;
+}
+
+int *locked_candidates(House hsA, House hsB, House hsC) {
+  int max_locked = MIN3(hsA[0], hsB[0], hsC[0]);
+
+  if (max_locked < 0) {
+    perror("locked_candidates Error: Invalid House(s).");
+    return NULL;
+  }
+
+  int *locked = (int *)malloc((max_locked + 1) * sizeof(int));
+  if (!locked) {
+    printf("locked_candidate Error: Failed to allocate memory.");
+    exit(EXIT_FAILURE);
+  }
+
+  locked[0] = 0; // First index reserved for candidate count.
+
+  for (int val = 1; val <= ORDER; val++) {
+    if (hsA[val] == 0 && hsB[val] == 0 && hsC[val] == 0) {
+      locked[locked[0] + 1] = val;
+      locked[0] += 1;
+    }
+  }
+
+  return locked;
+}
+
+// =========
+// Helpert methods
+// --------
+
+// Helper method for getting idx of square Entity given corresponding row/col
+int getBoxIdx(int row, int col, int size) {
+  int box_size = (int)sqrt((double)size);
+  return (row / box_size) * box_size + (col / box_size);
+}
+
+// =========
+// Display
+// --------
+
+void printValues(House hs) {
+  int val;
+  for (int i = 1; i <= ORDER; i++) {
+    val = (hs[i] > 0) ? i : 0;
+    printf("%d ", val);
+  }
+
+  if (hs[0] == INVALID) {
+    printf(" (House INVALID)");
+  }
+
+  printf("\n");
+}
+
+printCounts(House hs) {
+  for (int i = 1; i <= ORDER; i++) {
+    printf("%d ", hs[i]);
+  }
+  printf("  Candidate count: %d\n", hs[0]);
+}
+
+#endif // ENTITY_H
--- src/sudoku.c
+++ src/sudoku.c
@@ -1,53 +1,66 @@
 // Sudoku puzzle verifier and solver
 
+#include "entity.h"
 #include <assert.h>
+#include <math.h>
 #include <pthread.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-// takes puzzle size and grid[][] representing sudoku puzzle
-// and tow booleans to be assigned: complete and valid.
-// row-0 and column-0 is ignored for convenience, so a 9x9 puzzle
-// has grid[1][1] as the top-left element and grid[9]9] as bottom right
-// A puzzle is complete if it can be completed with no 0s in it
-// If complete, a puzzle is valid if all rows/columns/boxes have numbers from 1
-// to psize For incomplete puzzles, we cannot say anything about validity
-void checkPuzzle(int psize, int **grid, bool *complete, bool *valid) {
-  // YOUR CODE GOES HERE and in HELPER FUNCTIONS
-  *valid = true;
-  *complete = true;
-}
+int ORDER;
 
-// takes filename and pointer to grid[][]
-// returns size of Sudoku puzzle and fills grid
-int readSudokuPuzzle(char *filename, int ***grid) {
+pthread_mutex_t mutex;
+
+// takes filename and pointer to Grid of Houses
+// returns grid values of Sudoku puzzle and fills houses
+int **readSudokuPuzzle(char *filename, Grid *grid) {
   FILE *fp = fopen(filename, "r");
   if (fp == NULL) {
     printf("Could not open file %s\n", filename);
     exit(EXIT_FAILURE);
   }
+
   int psize;
   fscanf(fp, "%d", &psize);
-  int **agrid = (int **)malloc((psize + 1) * sizeof(int *));
+  ORDER = psize;
+  grid->order = ORDER;
+
+  for (int i = 0; i < NUM_HOUSE_TYPES; i++) {
+    grid->houses[i] = initEmptyHouses();
+  }
+
+  int **grid_vals = malloc(psize * sizeof(int *));
+
+  int val, box_idx;
   for (int row = 1; row <= psize; row++) {
-    agrid[row] = (int *)malloc((psize + 1) * sizeof(int));
+    grid_vals[row - 1] = malloc(psize * sizeof(int));
+
     for (int col = 1; col <= psize; col++) {
-      fscanf(fp, "%d", &agrid[row][col]);
+      fscanf(fp, "%d", &val);
+      grid_vals[row - 1][col - 1] = val;
+
+      if (val == 0) {
+        grid->complete = false;
+
+      } else {
+        box_idx = getBoxIdx(row - 1, col - 1, psize);
+
+        addValue(grid->houses[box_t_idx][box_idx], val);
+        addValue(grid->houses[row_t_idx][row - 1], val);
+        addValue(grid->houses[col_t_idx][col - 1], val);
+      }
     }
   }
   fclose(fp);
-  *grid = agrid;
-  return psize;
+  return grid_vals;
 }
 
-// takes puzzle size and grid[][]
-// prints the puzzle
-void printSudokuPuzzle(int psize, int **grid) {
-  printf("%d\n", psize);
-  for (int row = 1; row <= psize; row++) {
-    for (int col = 1; col <= psize; col++) {
-      printf("%d ", grid[row][col]);
+void printSudokuPuzzle(int **grid_vals, int order) {
+  printf("\n%d\n", order);
+  for (int row = 0; row < order; row++) {
+    for (int col = 0; col < order; col++) {
+      printf("%d ", grid_vals[row][col]);
     }
     printf("\n");
   }
@@ -56,11 +69,138 @@ void printSudokuPuzzle(int psize, int **grid) {
 
 // takes puzzle size and grid[][]
 // frees the memory allocated
-void deleteSudokuPuzzle(int psize, int **grid) {
-  for (int row = 1; row <= psize; row++) {
-    free(grid[row]);
+void deleteSudokuPuzzle(Grid *grid, int **grid_vals) {
+  for (int i = 0; i < grid->order; i++) {
+    free(grid_vals[i]);
+    grid_vals[i] = NULL;
   }
-  free(grid);
+  free(grid_vals);
+  grid_vals = NULL;
+
+  delGrid(grid);
+}
+
+// Don't need to worry about threads writing at the same time,
+// because they can only change bool to false,
+// and if even 1 house returns is invalid/incomplete,
+// the entire grid in invalid/incomplete.
+void *checkHousesStatus(void *arg) {
+  GridIterator *grid_it = (GridIterator *)arg;
+  Grid *grid = grid_it->grid;
+  int hs_t_idx = grid_it->house_type_idx;
+
+  if (!(grid->valid) && !(grid->complete)) {
+    free(grid_it);
+    return NULL;
+  } // Grid alrdy known to be invalid from other threads.
+
+  // Don't need to lock for reading
+  for (int hs_idx = 0; hs_idx < grid->order; hs_idx++) {
+    if (grid->houses[hs_t_idx][hs_idx][0] == INVALID && grid->valid) {
+      // Lock before changing shared invalid bool
+      pthread_mutex_lock(&mutex);
+      grid->valid = false;
+      pthread_mutex_unlock(&mutex);
+
+      printf("Found duplicate: hs_t_idx=%d, hs_idx=%d.\n", hs_t_idx, hs_idx);
+      // printf("House counts: ");
+      // printCounts(grid->houses[hs_t_idx][hs_idx]);
+      // printf("\n");
+
+    } else if (grid->houses[hs_t_idx][hs_idx][0] != FULL && grid->complete) {
+      // Locking before changing shared complete bool
+      pthread_mutex_lock(&mutex);
+      grid->complete = false;
+      pthread_mutex_unlock(&mutex);
+    }
+  }
+
+  free(grid_it);
+  return NULL;
+}
+
+void checkGridStatus(Grid *grid) {
+  for (int t_idx = 0; t_idx < NUM_HOUSE_TYPES; t_idx++) {
+    if (grid->houses[t_idx] == NULL) {
+      printf("Grid has a NULL Houses. Invalidating.\n");
+      grid->valid = false;
+      grid->complete = false;
+      return;
+    }
+  }
+
+  if (pthread_mutex_init(&mutex, NULL) != 0) {
+    printf("checkGridStatus Error: Mutex initialization failed.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Assume innocent until proven guilty.
+  grid->complete = true;
+  grid->valid = true;
+
+  pthread_t threads[3];
+  for (int i = 0; i < NUM_HOUSE_TYPES; i++) {
+    GridIterator *grid_it = newGridTypeIt(grid, i);
+
+    if (pthread_create(&threads[i], NULL, checkHousesStatus, grid_it) != 0) {
+      printf("checkHousesStatus Error: pthread_create failed.\n");
+      free(grid_it);
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  // Wait for all threads to complete
+  for (int i = 0; i < NUM_HOUSE_TYPES; i++) {
+    pthread_join(threads[i], NULL);
+  }
+}
+
+void solveCandidates(Grid *grid, int **grid_vals) {
+  // Candidate Counts (_cc) and indexes for each house
+  int b_cc, r_cc, c_cc, b_idx, r_idx, c_idx;
+  int *candidates;
+
+  int box_size = (int)sqrt(grid->order);
+
+  while (!(grid->complete)) {
+    for (b_idx = 0; b_idx < grid->order; b_idx++) {
+      b_cc = grid->houses[box_t_idx][b_idx][0];
+
+      if (b_cc > 0 && b_cc < 4) {
+        int start_row = (b_idx / box_size) * box_size;
+        int start_col = (b_idx % box_size) * box_size;
+
+        for (r_idx = start_row; r_idx < start_row + box_size; r_idx++) {
+          // r_cc = grid->houses[row_t_idx][r_idx][0];
+
+          for (c_idx = start_col; c_idx < start_col + box_size; c_idx++) {
+            // c_cc = grid->houses[col_t_idx][c_idx][0];
+
+            if (grid_vals[r_idx][c_idx] == 0) {
+              candidates = locked_candidates(grid->houses[box_t_idx][b_idx],
+                                             grid->houses[row_t_idx][r_idx],
+                                             grid->houses[col_t_idx][c_idx]);
+
+              if (candidates[0] == 1) {
+                setValue(candidates[1], *grid, r_idx, c_idx);
+                grid_vals[r_idx][c_idx] = candidates[1];
+              }
+
+              free(candidates);
+            }
+          }
+        }
+      }
+    }
+    checkGridStatus(grid);
+
+    if (!(grid->valid)) {
+      printf("solveCandidates ERROR: Grid invalidated during solve.\n");
+      break;
+    }
+
+    // printSudokuPuzzle(grid_vals, grid->order);
+  }
 }
 
 // expects file name of the puzzle as argument in command line
@@ -69,20 +209,31 @@ int main(int argc, char **argv) {
     printf("usage: ./sudoku puzzle.txt\n");
     return EXIT_FAILURE;
   }
-  // grid is a 2D array
-  int **grid = NULL;
-  // find grid size and fill grid
-  int sudokuSize = readSudokuPuzzle(argv[1], &grid);
-  bool valid = false;
-  bool complete = false;
-  checkPuzzle(sudokuSize, grid, &complete, &valid);
-  printf("Complete puzzle? ");
-  printf(complete ? "true\n" : "false\n");
-  if (complete) {
-    printf("Valid puzzle? ");
-    printf(valid ? "true\n" : "false\n");
+
+  Grid *grid = newGrid();
+
+  int **grid_vals = readSudokuPuzzle(argv[1], grid);
+  checkGridStatus(grid);
+
+  if (!grid->valid) {
+    printf("Puzzle invalid. An house cannot have duplicate values.\n");
+    deleteSudokuPuzzle(grid, grid_vals);
+    pthread_mutex_destroy(&mutex);
+    return EXIT_FAILURE;
+
+  } else {
+    printf("Puzzle valid.\n");
   }
-  printSudokuPuzzle(sudokuSize, grid);
-  deleteSudokuPuzzle(sudokuSize, grid);
+
+  if (!grid->complete) {
+    printSudokuPuzzle(grid_vals, grid->order);
+    printf("Puzzle incomplete. Proceeding to solve... \n\n");
+    solveCandidates(grid, grid_vals);
+  }
+
+  printf("Puzzle completed.\n");
+  printSudokuPuzzle(grid_vals, grid->order);
+  deleteSudokuPuzzle(grid, grid_vals);
+  pthread_mutex_destroy(&mutex);
   return EXIT_SUCCESS;
 }
