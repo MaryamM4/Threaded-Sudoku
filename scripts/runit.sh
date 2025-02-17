762aea65e39b8609bdde6a8e76948add076cf713
MaryamM4 <maryamma@outlook.com>
2025-02-17 04:48:54 +0000

    Added tests. Got stuck on puzzle9-fill-a.

--- scripts/runit.sh
+++ scripts/runit.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-PUZZLE_FILES=("puzzle2-fill-valid.txt" "puzzle2-invalid.txt" "puzzle2-valid.txt" "puzzle9-valid.txt")
-
 # Script to compile and run sudoku program
+
 rm -f sudoku
 gcc -g -fsanitize=address -Wall -Wextra -I../src ../src/sudoku.c -o sudoku -lpthread -lm
 
 
-for file in "${PUZZLE_FILES[@]}"; do
+for file in tests/puzzle*.txt; do
     echo "==========================="
     echo "Puzzle File: $file"
     echo
     ./sudoku "$file"
     echo 
 done
-
+echo "All tests complete."
 
 # to check for memory leaks, use
 # valgrind ./sudoku puzzle9-good.txt
